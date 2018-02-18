7ae8c8d54545ab3e496de11af7057b02c74c1b3b
gokselpirnal <gokselpirnal@gmail.com>
2018-02-18 11:00:25 +0300

    .

--- content.js
+++ content.js
@@ -1,5 +1,4 @@
 jQuery(document).ready(function () {
-   console.log(navigator.language || navigator.userLanguage)
    setTimeout(function(){
       jQuery('.irc_but_r').each(function(){
          jQuery(this).find('tbody tr td:first-child').after('<td><a class="i18192 r-iPWUDzUx2UVA see-image" role="button"><span>View Image</span></a></td>');
