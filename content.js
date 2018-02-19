b661d89cbddcda9712af129f5e6db1530a5c2d8c
gokselpirnal <gokselpirnal@gmail.com>
2018-02-19 09:52:47 +0300

    supports all google domains see:https://www.google.com/supported_domains

--- content.js
+++ content.js
@@ -4,10 +4,10 @@ document.onreadystatechange = function () {
          var btn = document.createElement('td');
          btn.innerHTML = '<a class="view-image" role="button"><span>View Image</span></a>';
          var firstBtn = elm.querySelector('.irc_but_r tbody tr td:first-child');
-         firstBtn.parentNode.insertBefore(btn, firstBtn.nextSibling)
+         firstBtn.parentNode.insertBefore(btn, firstBtn.nextSibling);
          elm.querySelector('.view-image').onclick = function () {
             window.open(elm.querySelector('.irc_mi').getAttribute('src'));
          }
       })
    }
-}
+};
